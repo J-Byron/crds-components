dbebfe455f1b5e1d0ed0b654ae1e0fe3d0c39605
Angelica Winkler <angelicawinkler@Angelicas-MacBook-Pro.local>
2019-12-03 12:47:31 -0500

    image upload updates state

--- src/components/crds-profile-pic-uploader/crds-profile-pic-uploader.tsx
+++ src/components/crds-profile-pic-uploader/crds-profile-pic-uploader.tsx
@@ -13,33 +13,49 @@ import Cropper from 'cropperjs';
 
 export class CrdsModal {
   @Prop() onSave: Function;
+  @State() uploadedImgUrl: string;
   // @Element() element: HTMLElement;
   private cropperImage: HTMLImageElement;
 
   componentDidLoad(){
+    this.renderCropper();
+  }
+
+  renderCropper(){
     const image = this.cropperImage;
     const cropper = new Cropper(image, {
-    aspectRatio: 16/1,
+    aspectRatio: 1/1,
      viewMode: 2,
       crop(event) {
         console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
       },
     });
   }
 
+  handleImageSelection(selectorFiles: FileList){
+      this.uploadedImgUrl = URL.createObjectURL(selectorFiles[0]);
+      setTimeout(function(){
+        const image = this.cropperImage;
+         debugger
+        const cropper = new Cropper(image, {
+        aspectRatio: 1/1,
+         viewMode: 2,
+          crop(event) {
+            console.log(event.detail.x);
+          },
+        });; }, 1000);
+  }
+
   render() {
+    const { uploadedImgUrl } = this;
+
     return (
       <div class="form-container">
-      <crds-button text="Choose image" color="blue"></crds-button>
         <div id="image-container">
-          <img class="cropper-hidden" ref={el => (this.cropperImage = el as HTMLImageElement)} id="cropper" src={"https://crds-media.imgix.net/4CWCvxN6iSb0QXDqvaT9Gt/a80cfd9a0b8154874777cbd71da59175/shutterstock_403980601.jpg?auto=format,compress&w=360&h=202.5&fit=crop"}/>
+          <img class="cropper-hidden" ref={el => (this.cropperImage = el as HTMLImageElement)} id="cropper" src={`${uploadedImgUrl? uploadedImgUrl : 'https://crds-media.imgix.net/4CWCvxN6iSb0QXDqvaT9Gt/a80cfd9a0b8154874777cbd71da59175/shutterstock_403980601.jpg?auto=format,compress&w=360&h=202.5&fit=crop' }`}/>
         </div>
+        <input type="file" name="pic" accept="image/*" onChange={(e) => this.handleImageSelection(e.target.files)}/>
+        <img src={this.uploadedImgUrl}/>
       </div>
 
     );
