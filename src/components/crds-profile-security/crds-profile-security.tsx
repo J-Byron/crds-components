576dd46920d1d09a884f3cfe1b9cf551b4b4edab
tatelucas <tate.lucas@gmail.com>
2020-01-22 14:55:45 -0500

    get email, pw values on email submit, validate them

--- src/components/crds-profile-security/crds-profile-security.tsx
+++ src/components/crds-profile-security/crds-profile-security.tsx
@@ -18,14 +18,29 @@ export class CrdsProfileSecurity {
   private user: ProfileSecurityUser = null;
 
   @State() displayName: string = null;
-  @State() value: string;
+  // @State() value: string;
   @Prop() defaultName: string;
   @State() currentEmail: string;
+  @State() newEmail: string;
+  @State() currentPassword: string;
+  @State() constraints = {
+    currentPassword: {
+      presence: true,
+      length: {
+        minimum: 6,
+        message: "must be at least 6 characters"
+      }
+    },
+    newEmail: {
+      email: true
+      }
+  };
+  @State() newPassword: string;
   @Element() public host: HTMLStencilElement;
-  
+
   public async componentWillLoad(){
     this.initToastr();
-    return CrdsApolloService.subscribeToApolloClient(); 
+    return CrdsApolloService.subscribeToApolloClient();
   }
 
   public componentWillRender() {
@@ -34,25 +49,11 @@ export class CrdsProfileSecurity {
 
   public componentDidRender() {
 
-    var constraints = {
-      password: {
-        presence: true,
-        length: {
-          minimum: 6,
-          message: "must be at least 6 characters"
-        }
-      },
-      currentEmail: {
-        email: true
-      },
-    };
-
-
     const renderedEvent = new CustomEvent('component rendered', {
       detail: this.host
     });
     document.dispatchEvent(renderedEvent);
-    console.log(validate({password: "bad", currentEmail: this.renderEmail()}, constraints));
+    //console.log(validate({password: "bad", currentEmail: this.renderEmail()}, constraints));
   }
 
   public getUser() {
@@ -89,7 +90,7 @@ export class CrdsProfileSecurity {
   // public handleFocus() {
   //   console.log('you clicked into the input 👌')
   // }
-  
+
   // public handleBlur() {
   //   console.log('you clicked out of the input 👋')
   // }
@@ -114,15 +115,39 @@ export class CrdsProfileSecurity {
     }
   }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    console.log(this.value);
-    toastr.success('Hey, we made it make toaster come up on form submission')
+  handlePasswordSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleEmailSubmit(e) {
+    e.preventDefault();
+
+
+    //
+    console.log(e.target);
+    console.log(this.host);
+    let newEmailInput = e.target.querySelectorAll("#newEmail")[0];
+    let currentPasswordInput = e.target.querySelectorAll("#currentPassword")[0];
+
+    console.log(newEmailInput.value);
+    console.log(currentPasswordInput.value);
+
+    if(validate({currentPassword: currentPasswordInput.value, currentEmail: newEmailInput.value}, this.constraints)) {
+      // If validate() returns anything, there are Errors
+      toastr.error('BAD');
+    } else {
+      // validate returned nothing, good to submit
+      toastr.success('Hey, we made it make toaster come up on form submission');
+    }
+
+    //console.log(this.currentPassword);
+
+
     // send data to our backend
   }
 
   handleChange(event) {
-    this.value = event.target.value;
+    console.log(event);
   }
 
   public render() {
@@ -130,32 +155,39 @@ export class CrdsProfileSecurity {
     return (
       <div class="row">
         <div class="col-sm-12 soft-ends">
-          <form onSubmit={(e) => this.handleSubmit(e)}>
+          <form onSubmit={(e) => this.handleEmailSubmit(e)}>
             <h3 class="component-header">Change Email</h3>
             <div class="input-group">
-              <label>Email</label>
-              <input class="form-control" type="email" id="currentEmail" name="currentEmail" placeholder="Enter email address" 
-               value={this.value} onInput={(event) => this.handleChange(event)} />
+              <label>Current Email</label>
+              <input class="form-control" type="email" id="currentEmail" name="currentEmail" placeholder="Enter email address"
+               value={this.user.email} disabled />
             </div>
+            <div class="input-group">
+              <label>New Email</label>
+              <input class="form-control" type="email" id="newEmail" name="newEmail" placeholder="Enter email address"
+               value="" onInput={(event) => this.handleChange(event)}  required />
+            </div>
+            <div class="input-group">
+              <label>Current Password</label>
+              <input class="form-control" type="password" id="currentPassword" name="currentPassword" placeholder="Current password" value="" required />
+            </div>
+            <button class="btn btn-medium btn-blue" type="submit" value="Submit">Update Email</button><span class="label push-left">{this.value}</span>
+          </form>
+        </div>
+        <div class="col-sm-12 soft-ends">
+          <form onSubmit={(e) => this.handlePasswordSubmit(e)}>
+            <h3 class="component-header">Change Password</h3>
             <div class="input-group">
               <label>Current Password</label>
               <input class="form-control" type="password" id="current-password" name="password" placeholder="Current password"/>
             </div>
-            <button class="btn btn-medium btn-blue" type="submit" value="Submit">Save</button><span class="label push-left">{this.value}</span>
+            <div class="input-group">
+              <label>New Password</label>
+              <input class="form-control" type="password" id="new-password" name="password" placeholder="New password"/>
+              <input class="form-control" type="password" id="confirm-password" name="password" placeholder="Confirm new password"/>
+            </div>
+            <button class="btn btn-medium btn-blue" type="submit" value="Submit">Update Password</button>
           </form>
-        </div>
-        <div class="col-sm-12 soft-ends">
-          <h3 class="component-header">Change Password</h3>
-          <div class="input-group">
-            <label>Current Password</label>
-            <input class="form-control" type="password" id="current-password" name="password" placeholder="Current password"/>
-          </div>
-          <div class="input-group">
-            <label>New Password</label>
-            <input class="form-control" type="password" id="new-password" name="password" placeholder="New password"/>
-            <input class="form-control" type="password" id="confirm-password" name="password" placeholder="Confirm new password"/>
-          </div>
-          <button class="btn btn-medium btn-blue">Save</button>
         </div>
       </div>
     );
